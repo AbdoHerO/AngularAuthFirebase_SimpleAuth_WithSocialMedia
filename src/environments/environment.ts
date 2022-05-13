c8f81b44ad4775097597347a5101b74605c7ce01
Abderrahmane OUHNA <abderrahmane.ouhna@gmail.com>
2022-05-13 21:58:50 +0100

    init projet - first version - simple auth + auth with google

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDjOaGf0lXqRUYLQKSNvGpZO-Fq7t60UFg",
+    authDomain: "angularfiebase-authentic-baabb.firebaseapp.com",
+    projectId: "angularfiebase-authentic-baabb",
+    storageBucket: "angularfiebase-authentic-baabb.appspot.com",
+    messagingSenderId: "687095009326",
+    appId: "1:687095009326:web:3c5bd9cbbad9672d6e4fbf",
+    measurementId: "G-NGGMZQ8ZSN"
+  }
 };
 
 /*
